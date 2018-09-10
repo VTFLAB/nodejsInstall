19afd70cc959bef81086a6267179901d56c3d285
VTFLAB <satellite.doll@gmail.com>
2018-09-11 08:16:47 +0900

    Update install.sh
    fix

--- install.sh
+++ install.sh
@@ -12,11 +12,12 @@ sudo npm cache clean
 sudo npm install n -g
 
 #n packageを利用して最新のnodeをインストール
-sudo n stable
+#※n packageでnode10.10がうまく行かない？ 2018/09/11最新安定の8.11.4でとりま対処、v10~がうまく導入できないっぽい
+sudo n 8.11.4
 sudo ln -sf /usr/local/bin/node /usr/bin/node
 
-#初回に入れたnodejs,npm古いので混乱を避けるために削除 ※n packageでnode10.10がうまく行かない？
-#sudo apt purge -y nodejs npm
+#初回に入れたnodejs,npm古いので混乱を避けるために削除
+sudo apt purge -y nodejs npm
 
 #現在のバージョン情報を吐き出す
 node -v > node_version.txt
