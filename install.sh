ea1dc5d48898ee47dfd1592a4ebeca02ada57d6a
VTFLAB <satellite.doll@gmail.com>
2018-09-11 08:02:52 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# NodeJS & npm for n package
+
+#いつもの
+sudo apt update && sudo apt upgrade
+
+#通常通りNodeJSとnpmのインストール
+sudo apt install -y nodejs npm
+
+#n package導入
+sudo npm cache clean
+sudo npm install n -g
+
+#n packageを利用して最新のnodeをインストール
+sudo n stable
+sudo ln -sf /usr/local/bin/node /usr/bin/node
+
+#初回に入れたnodejs,npm古いので混乱を避けるために削除
+sudo apt purge -y nodejs npm
+
+#現在のバージョン情報を吐き出す
+node -v > node_version.txt
+
+#備考
+#別バージョンを管理したい場合は以下のように切り替えることが可能
+#$ sudo n 4.2.3
+#$ node -v
+#v4.2.3
